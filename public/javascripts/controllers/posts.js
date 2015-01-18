27741c46aa69a8ef8f3261b1b77ba2cb1f0b55c5
Gene Lee <genejaelee@gmail.com>
2015-01-18 14:07:44 -0800

    header from x-amz-signature to just signature

--- public/javascripts/controllers/posts.js
+++ public/javascripts/controllers/posts.js
@@ -111,14 +111,14 @@ postsModule.controller('postsNewController', function(uploaderMethods, $scope, $
           url: 'https://genejaelee-assets.s3.amazonaws.com/',
           method: 'POST',
           data: {
-            key: file.name,
-            AWSAccessKeyId: credentials.key,
-            acl: 'public-read',
-            policy: credentials.policy,
-            'x-amz-algorithm': 'AWS4-HMAC-SHA256',
-            'x-amz-signature': credentials.signature,
+            'key': file.name,
+            'AWSAccessKeyId': credentials.key,
+            'acl': 'public-read',
+            'policy': credentials.policy,
+            'algorithm': 'AWS4-HMAC-SHA256',
+            'signature': credentials.signature,
             "Content-Type": file.type != '' ? file.type : 'application/octet-stream',
-            filename: file.name
+            'filename': file.name
           },
           file: file,
         }).
