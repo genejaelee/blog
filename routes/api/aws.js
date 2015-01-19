bd2d8b6450e3fe7d49434bb6fed2f082cc7ddbdf
Gene Lee <genejaelee@gmail.com>
2015-01-18 16:12:06 -0800

    update hashing order for aws v4

--- routes/api/aws.js
+++ routes/api/aws.js
@@ -8,6 +8,7 @@ var awsApi = function(app, db) {
     var awsSecret = process.env.AWS_SECRET_ACCESS_KEY;
     var awsAccessKey = process.env.AWS_ACCESS_KEY_ID;
     var date = moment(Date.now()).format('YYYYMMDD');
+    var region = "us-west-1";
     var credential = awsAccessKey + "%2F" + date + "%2F" + "us-west-1%2Fs3%2Faws4_request"
     var policyJson = {
       "expiration": "2020-01-01T00:00:00Z",
@@ -24,7 +25,7 @@ var awsApi = function(app, db) {
     }
     var encodedPolicy = Buffer(JSON.stringify(policyJson)).toString('base64');
     // create HMAC-SHA256 hash
-    var signature = generateHmac(encodedPolicy, awsSecret);
+    var signature = generateHmac(awsSecret, date, region, "s3", encodedPolicy);
     
     res.json({
       'key': awsAccessKey,
@@ -36,10 +37,14 @@ var awsApi = function(app, db) {
   });
 }
 
-function generateHmac (data, awsSecret, algorithm, encoding) {
+function generateHmac (awsSecret, date, region, service, encodedPolicy, algorithm, encoding) {
   encoding = encoding || "base64";
   algorithm = algorithm || "sha256";
-  return crypto.createHmac(algorithm, awsSecret).update(data).digest(encoding);
+  var hash1 = crypto.createHmac(algorithm, "AWS4" + awsSecret).update(date);
+  var hash2 = crypto.createHmac(algorithm, hash1).update(region);
+  var hash3 = crypto.createHmac(algorithm, hash2).update(service);
+  var hash4 = crypto.createHmac(algorithm, hash3).update("aws4_request");
+  return crypto.createHmac(algorithm, hash4).update(encodedPolicy).digest(encoding);
 }
 
 module.exports = awsApi;
